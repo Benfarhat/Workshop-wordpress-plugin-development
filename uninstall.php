b04f23130ab58b71e65d94bc21ffdac73a7895ab
Benfarhat <benfarhat.elyes@gmail.com>
2018-06-19 20:27:47 +0100

    initial commit

---
+++ uninstall.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Trigger this file on Plugin uninstall
+ * 
+ * @package WorkshopWPD
+ */
+
+ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    die;
+ }
+
+ // Clear Database stored data
+
+ 
