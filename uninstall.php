38de081f8dac539ff170281f5f7412f3c6f4c4d9
Benfarhat <benfarhat.elyes@gmail.com>
2018-06-19 22:16:37 +0100

    clean database on uninstall

--- uninstall.php
+++ uninstall.php
@@ -12,4 +12,26 @@
 
  // Clear Database stored data
 
- 
+ /*
+ // Method 1:
+$books = get_posts( array( 'post_type' => 'book', 'numberposts' => -1 ) );
+
+foreach( $books as $book ) {
+    wp_delete_post( $book->ID , true );
+}
+*/
+
+// Method 2:
+global $wpdb;
+/*
+// Original code
+$wpdb->query( "DELETE FROM ".$wpdb->prefix."posts WHERE post_type = 'book'" );
+$wpdb->query( "DELETE FROM ".$wpdb->prefix."postmeta WHERE post_id NOT IN (SELECT id FROM ".$wpdb->prefix."posts)" );
+$wpdb->query( "DELETE FROM ".$wpdb->prefix."term_relationships WHERE object_id NOT IN (SELECT id FROM ".$wpdb->prefix."posts)" );
+*/
+
+// Alternative code
+$wpdb->query( "DELETE FROM ".$wpdb->prefix."postmeta WHERE post_id IN (SELECT id FROM ".$wpdb->prefix."posts WHERE post_type = 'book')" );
+$wpdb->query( "DELETE FROM ".$wpdb->prefix."term_relationships WHERE object_id IN (SELECT id FROM ".$wpdb->prefix."posts WHERE post_type = 'book')" );
+$wpdb->query( "DELETE FROM ".$wpdb->prefix."posts WHERE post_type = 'book'" );
+
