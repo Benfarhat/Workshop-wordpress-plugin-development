b094a1025cfe202112d6cdc1782c47c66297f3e5
Benfarhat <benfarhat.elyes@gmail.com>
2018-06-20 18:26:02 +0100

    Verify if class exists

--- workshop.php
+++ workshop.php
@@ -36,91 +36,95 @@ if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
     require dirname( __FILE__ ) . '/vendor/autoload.php';
 }
 
-class WorkshopWPD
-{
+if ( !class_exists( 'WorkshopWPD' ) ) {
 
-    public $name;
+    class WorkshopWPD
+    {
 
-    public function __construct(){
-        $this->name = plugin_basename( __FILE__ );
+        public $name;
+
+        public function __construct(){
+            $this->name = plugin_basename( __FILE__ );
+        }
+
+        function register(){
+            //add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
+            // If we use it in a static way, $this will not work,
+            // We need to change enqueue function as static
+            add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
+
+            add_action( 'admin_menu', array( $this, 'add_admin_pages'  ) );
+
+            add_filter( "plugin_action_links_$this->name", array( $this, 'settings_link' ) ) ;
+        }
+
+        public function settings_link( $links ){
+            // add custom settings link
+            $settings_link = '<a href="admin.php?page=workshopwpd">'.__( 'Settings', 'WorkshopWPD' ).'</a>';
+            array_push( $links, $settings_link );
+            return $links;
+            
+
+        } 
+
+        public function add_admin_pages() {
+            add_menu_page( 'WorkshopWPD', 'WorkshopWPD', 'manage_options', 'workshopwpd', array( $this, 'admin_index' ), 'dashicons-store', 110 );
+        }
+
+        public function admin_index() {
+            require_once plugin_dir_path( __FILE__ ) . 'templates/admin.php';
+        }
+
+        protected function create_post_type() {
+            add_action( 'init', array( $this, 'custom_post_type' ) );
+        }
+        /*
+        function register_admin_scripts(){
+            // Admin
+            add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
+        }
+
+        function register_scripts(){
+            // Front
+            add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
+        }
+        */
+
+        /*
+
+        function activate(){
+            // generated a CPT
+            $this->custom_post_type();
+            // flush rewrite rules
+            flush_rewrite_rules();
+        }
+
+        function deactivate(){
+            // flush rewrite rules
+            flush_rewrite_rules();
+        }
+
+        */
+        /*
+        function uninstall(){ // We'll use uninstall.php
+            // delete CPT
+            // delete all the plugin data from the DB
+        }
+        */
+
+        function custom_post_type(){
+            register_post_type( 'book', ['public' => true, 'label' => 'Books'] );
+        }
+
+        static function enqueue(){
+            // enqueue all our scripts
+            wp_enqueue_style( 'wwpdstyle', plugins_url( '/assets/css/style.css', __FILE__ ) );
+            wp_enqueue_script( 'wwpdscript', plugins_url( '/assets/js/script.js', __FILE__ ) );
+
+        }
+
+
     }
-
-    function register(){
-        //add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
-        // If we use it in a static way, $this will not work,
-        // We need to change enqueue function as static
-        add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
-
-        add_action( 'admin_menu', array( $this, 'add_admin_pages'  ) );
-
-        add_filter( "plugin_action_links_$this->name", array( $this, 'settings_link' ) ) ;
-    }
-
-    public function settings_link( $links ){
-        // add custom settings link
-        $settings_link = '<a href="admin.php?page=workshopwpd">'.__( 'Settings', 'WorkshopWPD' ).'</a>';
-        array_push( $links, $settings_link );
-        return $links;
-        
-
-    } 
-
-    public function add_admin_pages() {
-        add_menu_page( 'WorkshopWPD', 'WorkshopWPD', 'manage_options', 'workshopwpd', array( $this, 'admin_index' ), 'dashicons-store', 110 );
-    }
-
-    public function admin_index() {
-        require_once plugin_dir_path( __FILE__ ) . 'templates/admin.php';
-    }
-
-    protected function create_post_type() {
-        add_action( 'init', array( $this, 'custom_post_type' ) );
-    }
-    /*
-    function register_admin_scripts(){
-        // Admin
-        add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
-    }
-
-    function register_scripts(){
-        // Front
-        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
-    }
-    */
-
-    /*
-
-    function activate(){
-        // generated a CPT
-        $this->custom_post_type();
-        // flush rewrite rules
-        flush_rewrite_rules();
-    }
-
-    function deactivate(){
-        // flush rewrite rules
-        flush_rewrite_rules();
-    }
-
-    */
-    /*
-    function uninstall(){ // We'll use uninstall.php
-        // delete CPT
-        // delete all the plugin data from the DB
-    }
-    */
-
-    function custom_post_type(){
-        register_post_type( 'book', ['public' => true, 'label' => 'Books'] );
-    }
-
-    static function enqueue(){
-        // enqueue all our scripts
-        wp_enqueue_style( 'wwpdstyle', plugins_url( '/assets/css/style.css', __FILE__ ) );
-        wp_enqueue_script( 'wwpdscript', plugins_url( '/assets/js/script.js', __FILE__ ) );
-
-    }
-
 
 }
 
