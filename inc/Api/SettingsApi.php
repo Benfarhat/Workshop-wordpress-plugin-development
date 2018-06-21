9a643e6e5143ef4398f87e0c08807f6aa524898e
Benfarhat <benfarhat.elyes@gmail.com>
2018-06-21 17:30:05 +0100

    add subpages

--- inc/Api/SettingsApi.php
+++ inc/Api/SettingsApi.php
@@ -8,10 +8,11 @@ namespace Inc\Api;
 class SettingsApi 
 {
     public $admin_pages = array();
+    public $admin_subpages = array();
 
     public function register() {
         if ( ! empty( $this->admin_pages ) ) {
-            add_action( 'admin_menu', array( $this, 'addAdminMenu' ) )
+            add_action( 'admin_menu', array( $this, 'addAdminMenu' ) );
         }
     }
 
@@ -22,10 +23,61 @@ class SettingsApi
 
     }
 
-    private function addAdminMenu() {
+    public function withSubPage( string $title = null ) {
+        if ( empty( $this->admin_pages ) ) {
+            return $this;
+        }
+
+        $admin_pages = $this_pages[0];
+
+        $subpage = array (
+			array( 
+                'parent_slug' => $admin_page['menu_slug'], 
+				'page_title' => $admin_page['page_title'], 
+				'menu_title' => $admin_page['menu_title'], 
+				'capability' => $admin_page['capability'], 
+				'menu_slug' => $admin_page['menu_slug'], 
+				'callback' => function() { echo "<h1>ma page</h1>"; }
+            )
+        );
+        
+        $this->admin_subpages = $subpage;
+
+        return $this;
+    }
+
+    public function addSubPages( array $pages ){
+
+        // @todo
+        $this->admin_subpages = array_merge( $this->admin_subpages, $pages);
+
+        return $this;
+
+    }
+
+    public function addAdminMenu() {
 
         foreach ( $this->admin_pages as $page ) {
-            add_menu_page( $page['page_title'], $page['menu_title'], $page['capability'], $page['menu_slug'], $page['function'], $page['icon_url'], $page['position'] )
+            add_menu_page( 
+                $page['page_title'], 
+                $page['menu_title'], 
+                $page['capability'], 
+                $page['menu_slug'], 
+                $page['callback'], 
+                $page['icon_url'], 
+                $page['position'] 
+            );
+        }
+
+        foreach ( $this->admin_subpages as $page ) {
+            add_submenu_page( 
+                $page['parent_slug'], 
+                $page['page_title'], 
+                $page['menu_title'], 
+                $page['capability'], 
+                $page['menu_slug'], 
+                $page['callback']
+            );
         }
 
     }
--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -6,14 +6,62 @@
 namespace Inc\Pages;
 
 use \Inc\Base\BaseController;
+use \Inc\Api\SettingsApi;
 
 class Admin extends BaseController
 {
 
-	public function register() {
-		add_action( 'admin_menu', array( $this, 'add_admin_pages'  ) );
+	public $settings;
+    public $pages = array();
+    public $subpages = array();
+
+	public function __construct(){
+		$this->settings = new SettingsApi;
+
+		$this->pages = array(
+			array( 
+				'page_title' => 'WorkshopWPD',
+				'menu_title' => 'WorkshopWPD',
+				'capability' => 'manage_options',
+				'menu_slug' => 'workshopwpd',
+				'callback' => function() {echo "<h1>ma page</h1>"; },
+				'icon_url' => 'dashicons-admin-site',
+				'position' => 50
+			),
+			array( 
+				'page_title' => 'WorkshopWPD2',
+				'menu_title' => 'WorkshopWPD2',
+				'capability' => 'manage_options',
+				'menu_slug' => 'workshopwpd2',
+				'callback' => function() {echo "<h1>ma page</h1>"; },
+				'icon_url' => 'dashicons-admin-site',
+				'position' => 50
+			),
+		);
+
+		$admin_pages = $this_pages[0];
+
+        $this->subpage = array (
+			array( 
+                'parent_slug' => $admin_page['menu_slug'], 
+				'page_title' => $admin_page['page_title'], 
+				'menu_title' => $admin_page['menu_title'], 
+				'capability' => $admin_page['capability'], 
+				'menu_slug' => $admin_page['menu_slug'], 
+				'callback' => function() { echo "<h1>ma page</h1>"; }
+            )
+		);
+		
 	}
 
+	public function register() {
+
+
+		$this->settings->addPages( $this->pages )->withSubPage( __('Dashboard', WorkshopWPD) )->addSubPages( $this->subpages )->register();
+		//add_action( 'admin_menu', array( $this, 'add_admin_pages'  ) );
+	}
+
+	/*
 	public function add_admin_pages() {
 		add_menu_page( 'WorkshopWPD', 'WorkshopWPD', 'manage_options', 'workshopwpd', array( $this, 'admin_index' ), 'dashicons-admin-site', 50 );
 	}
@@ -22,4 +70,5 @@ class Admin extends BaseController
 		require_once $this->plugin_path . 'templates/admin.php';
 	}	
 		
+	*/
 }
