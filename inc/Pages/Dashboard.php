172caee918ba82894643c3e270e06b4b6851c523
Benfarhat <benfarhat.elyes@gmail.com>
2018-06-30 12:15:30 +0100

    Change class name Admin to Dashboard

--- inc/Pages/Dashboard.php
+++ inc/Pages/Dashboard.php
@@ -10,7 +10,7 @@ use Inc\Base\BaseController;
 use Inc\Api\Callbacks\AdminCallbacks;
 use Inc\Api\Callbacks\ManagerCallbacks;
 
-class Admin extends BaseController
+class Dashboard extends BaseController
 {
 
 	public $settings;
